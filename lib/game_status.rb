b211fd99f3024314f51d5953c912557b804eedc2
celebrated-ninja-3247 <cnunlee7013@gmail.com>
2019-12-15 17:26:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -12,8 +12,8 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
-  if WIN_COMBINATIONS = [0,1,2]
-    WIN_COMBINATIONS(won) 
+  if WIN_COMBINATIONS == [0,1,2] && [3,4,5] && [6,7,8] && [0,3,6] && [1,4,7] && [2,5,8] && [0,4,8] && [2,4,6] 
+    return #won?
   end
 end
 
